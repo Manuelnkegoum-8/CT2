b9704abe1f92333608713639285a5ed8a8b22766
Manuel NKEGOUM <81433245+Manuelnkegoum-8@users.noreply.github.com>
2024-07-02 16:55:47 +0200

    Update trainer.py

--- utils/trainer.py
+++ utils/trainer.py
@@ -4,21 +4,29 @@ import torch.nn.functional as F
 
 
 def compute_loss(preds_q,pred_ab,soft_labels,img_ab,weight_l1=10.,criterion=None):
+        
         B,C,h,w = preds_q.size()
-        preds_q = F.softmax(preds_q.permute(0,2,3,1).view(-1,C),dim=-1)
+        preds = preds_q.permute(0,2,3,1).view(-1,C)
+        probs = F.softmax(preds,dim=-1)
         soft_labels = soft_labels.permute(0,2,3,1).view(-1,C)
-        tmp = preds_q.clone()
+        tmp = probs.clone()
         tmp2 = soft_labels.clone()
-        loss = (torch.log(preds_q.masked_fill(tmp==0,1))-torch.log(soft_labels.masked_fill(tmp2==0,1)))*soft_labels
-        loss = -loss.sum()/(B*h*w)
+        
+        probs = probs.masked_fill(tmp==0,1)
+        log_probs = torch.log(probs)
+        tmp2 = tmp2.masked_fill(soft_labels==0,1)
+        labels = torch.log(tmp2)
+
+        ll = (log_probs-labels)*soft_labels
+        loss = -torch.sum(ll)/(B*h*w)
         l1_loss = criterion(pred_ab,img_ab)
-        #print("\n",pred_ab)
-        print("\n",loss)
-        print("\n",l1_loss)
+        """print("\n",loss)
+        print("\n",l1_loss)"""
         return loss+weight_l1*l1_loss
 
 def train_epoch(model,dataloader,optimizer,weight_l1,criterion,device):
     avg_loss = 0.0
+    k = 0
     model.train()
     with tqdm(dataloader,unit='batch',ncols=80,colour='blue') as tepoch:
         for n,data in enumerate(tepoch):
@@ -27,19 +35,21 @@ def train_epoch(model,dataloader,optimizer,weight_l1,criterion,device):
             bs = img_L.size(0)
             pred_q,pred_ab,soft_labels,final_img = model(img_L,img_ab,img_mask,True)
             loss = compute_loss(pred_q,pred_ab,soft_labels,img_ab,weight_l1,criterion)
-            loss = loss/16
-            
+            #loss = loss/16
+            optimizer.zero_grad()
             loss.backward()
-            if n%16==0:
-                optimizer.step()
-                optimizer.zero_grad()
-                tepoch.set_postfix(loss=loss.item())
+            #if n%1==0:
+            optimizer.step()
+            
+            tepoch.set_postfix(loss=loss.item())
             avg_loss+=loss.item()*bs
-    return avg_loss/(n+1)
+            k+=bs
+    return avg_loss/k
 
 @torch.no_grad()
 def validate(model,dataloader,weight_l1,criterion,device):
     avg_loss = 0.0
+    k = 0
     model.eval()
     with tqdm(dataloader,unit='batch',ncols=80,colour='green') as tepoch:
         for n,data in enumerate(tepoch):
@@ -50,4 +60,5 @@ def validate(model,dataloader,weight_l1,criterion,device):
             loss = compute_loss(pred_q,pred_ab,soft_labels,img_ab,weight_l1,criterion)
             tepoch.set_postfix(loss=loss.item())
             avg_loss+=loss.item()*bs
-    return avg_loss/(n+1)
+            k+=bs
+    return avg_loss/k
